bcf7b0c535aab1551b34a87ac2f286d172d1cb05
web-adi <adityamondal282@gmail.com>
2024-12-28 00:13:30 +0530

    new changes

--- src/db/index.js
+++ src/db/index.js
@@ -6,11 +6,12 @@ const connectDB = async () => {
     const connectionIntance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
+
     console.log(
-      ` MongoDB connected !! BD HOST: ${connectionIntance.connection.host}`
+      `\n MongoDB connect !! DB HOST : ${connectionIntance.connection.host}`
     );
   } catch (error) {
-    console.error("Mongodb connection error : ", error);
+    console.error("MongoDB connection error : ", error);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-// require('dotenv').config()
-import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/index.js";
+import dotenv from "dotenv";
 
 dotenv.config({
   path: "./env",
 });
-
-connectDB();
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//   } catch (error) {
-//     console.error("ERROR:", error);
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection field !!", err);
+  });
